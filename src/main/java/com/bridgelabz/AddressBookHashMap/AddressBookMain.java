affc186c402857a33405c3aff9bcc5f5bf61d4c0
Nandini Yarra <yarranandhini1416@gmail.com>
2022-01-07 20:23:26 +0530

    UPDATED:adding new contact

--- src/main/java/com/bridgelabz/AddressBookHashMap/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookHashMap/AddressBookMain.java
@@ -3,8 +3,9 @@ package main.java.com.bridgelabz.AddressBookHashMap;
 public class AddressBookMain {
 	public static void main(String[] args) {
 
-		InterfaceOne person1 = new Person();
-		person1.createContact();
-		person1.display();
+			InterfaceOne person1 = new Person();
+			person1.createContact();
+			person1.addContact();
+			person1.display();
+		}
 	}
-}
--- src/main/java/com/bridgelabz/AddressBookHashMap/Person.java
+++ src/main/java/com/bridgelabz/AddressBookHashMap/Person.java
@@ -23,9 +23,44 @@ public class Person implements InterfaceOne  {
 			detailsBook.put("two",new Contact("Prudhvi","raj","cycle colony", "SaiDurga", "Ap", 11, 8676247876,"prudhvi@two.com"));
 			detailsBook.put("three",new Contact("Bablu","kumar","Hb colony","Ts", 20, 8328606571,"bablu@three.com"));
 		}        
+		public void addContact()  {
+	    	System.out.println("Enter how many contacts you want to add");
+	    	int numOfContacts = sc.nextInt();
+	    	while(numOfContacts > 0) {
+		    	System.out.println("Enter id of person");
+				String id = sc.next();
+		    	if(detailsBook.containsKey(id)) {
+		    		System.out.println("Contact already exists!");
+		    		return;
+		    	}
+		    	else {
+		    		System.out.println("Enter First Name :");
+		        	String fName = sc.next();
+		        	System.out.println("Enter Last Name :");
+		        	String lName = sc.next();
+		        	System.out.println("Enter Address :");
+		        	String address = sc.next();
+		        	System.out.println("Enter City :");
+		        	String city = sc.next();
+		        	System.out.println("Enter State :");
+		        	String state = sc.next();
+		        	System.out.println("Enter Zip :");
+		        	int zip = sc.nextInt();
+		        	System.out.println("Enter Phone Number :");
+		        	long number = sc.nextLong();
+		        	System.out.println("Enter Email :");
+		        	String mail = sc.next();
+		    		detailsBook.put(id,new Contact(fName, lName, address, city, state, zip, number, mail));
+		    		numOfContacts--;
+		    	}
+	    	}
+	    }
+	        
 	    public void display() {
 	        System.out.println("Created contact list is");
 	        for(Map.Entry m : detailsBook.entrySet()) {
 	        	System.out.println(m.getKey()+"--> "+m.getValue());
 	        }
 	    }
+	   
+	}
