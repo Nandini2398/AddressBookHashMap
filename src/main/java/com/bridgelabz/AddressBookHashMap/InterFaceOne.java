d7c54a56ce58a1a2f40a30eba75ade4eaf1b6f66
Nandini Yarra <yarranandhini1416@gmail.com>
2022-01-07 20:07:59 +0530

    UPDATED:for address book it is having unique entries

---
+++ src/main/java/com/bridgelabz/AddressBookHashMap/InterFaceOne.java
@@ -0,0 +1,12 @@
+package main.java.com.bridgelabz.AddressBookHashMap;
+
+public interface InterfaceOne {
+	void createContact();
+	void addContact();
+	void editContact();
+	void deleteContact();
+	void display();
+	void displayBook();
+	void addToBook();
+	void operation();
+}
