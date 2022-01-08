3667f414b95e4750fa8946a8225b14eac031915a
Nandini Yarra <yarranandhini1416@gmail.com>
2022-01-08 08:33:08 +0530

    UPDATED:by adding multiple address books

--- src/main/java/com/bridgelabz/AddressBookHashMap/Person.java
+++ src/main/java/com/bridgelabz/AddressBookHashMap/Person.java
@@ -4,108 +4,101 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Person implements InterfaceOne  {
-	import java.util.HashMap;
-	import java.util.Map;
-	import java.util.Scanner;
-
-	import java.util.HashMap;
-	import java.util.Map;
-	import java.util.Scanner;
-	public class Person implements InterfaceOne  {
-	    static Scanner sc = new Scanner(System.in);
-	    static String firstName,lastName,address,state,email,city;
-	    static int zip;
-	    static long phoneNumber;
-	    ContactInfo infoContact = new ContactInfo();
-	    Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
-	    Map<String,Contact> detailsBook = new HashMap<>();
-		static Map<String,HashMap<String,Contact>> book = new HashMap<>();
-		public void createContact() {
-			System.out.println("Enter how many contacts you want to create");
-	    	int numOfContact = sc.nextInt();
-	    	while(numOfContact > 0) {
-				System.out.println("Enter Name of person");
-				String firstName = sc.next();
-		    	if(!detailsBook.containsKey(firstName)) {
-		    		infoContact.info();
-					detailsBook.put(firstName,new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email));
-					numOfContact--;
-		    	}
-		    	else {
-		    		System.out.println("Contact already exists!");
-		    	}
+public class Person implements InterFaceOne  {
+    static Scanner sc = new Scanner(System.in);
+    static String firstName,lastName,address,state,email,city;
+    static int zip;
+    static long phoneNumber;
+    ContactInfo infoContact = new ContactInfo();
+    Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+    Map<String,Contact> detailsBook = new HashMap<>();
+	static Map<String,HashMap<String,Contact>> book = new HashMap<>();
+	public void createContact() {
+		System.out.println("Enter how many contacts you want to create");
+    	int numOfContact = sc.nextInt();
+    	while(numOfContact > 0) {
+			System.out.println("Enter Name of person");
+			String firstName = sc.next();
+	    	if(!detailsBook.containsKey(firstName)) {
+	    		infoContact.info();
+				detailsBook.put(firstName,new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email));
+				numOfContact--;
 	    	}
-		}
-	    public void addContact()  {
-	    	System.out.println("Enter how many contacts you want to add");
-	    	int numOfContacts = sc.nextInt();
-	    	while(numOfContacts > 0) {
-		    	System.out.println("Enter Name of person");
-				String firstName = sc.next();
-		    	if(detailsBook.containsKey(firstName)) {
-		    		System.out.println("Contact already exists!");
-		    		return;
-		    	}
-		    	else {
-		    		infoContact.info();
-		    		detailsBook.put(firstName,new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email));
-		    		numOfContacts--;
-		    	}
+	    	else {
+	    		System.out.println("Contact already exists!");
 	    	}
-	    }
-	    public void editContact()  {
-	    	System.out.println("Enter name of person whose contact you want to edit");
-				String firstName = sc.next();
-		    	if(detailsBook.containsKey(firstName)) {
-		        	infoContact.info();
-		    		detailsBook.put(firstName,new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email));
-		    	}
-		    	else {
-		    		System.out.println("Contact is not present in book");
-		    		return;
-		    	}
-	    }
-	    public void deleteContact()  {
-	    	System.out.println("Enter name of person whose contact you want to delete");
-				String name = sc.next();
-		    	if(detailsBook.containsKey(name)) {
-		    		detailsBook.remove(name);
-		    	}
-		    	else {
-		    		System.out.println("Contact is not present in book");
-		    		return;
-		    	}
-	    }   
-	    public void display() {
-	        System.out.println("Created contact list is");
-	        for(Map.Entry m : detailsBook.entrySet()) {
-	        	System.out.println(m.getKey()+"--> "+m.getValue());
-	        }
-	    }
-	    public void addToBook() {
-		    	System.out.println("Enter Name of Address Book");
-		    	String bookName = sc.next();
-		    	if(!book.containsKey(bookName)) {
-			    	book.put(bookName, (HashMap<String, Contact>) detailsBook);
-			    	}
-		    	else {
-		    		System.out.println("Book already exists!");
-		    	}
-	    }
-	    public void operation() {
-	    	System.out.println("Enter how many address books you want to add");
-	    	int noOfAddressBook = sc.nextInt();
-	    	while(noOfAddressBook > 0) {
-	    		addContact();
-	    		addToBook();
-	    		noOfAddressBook--;
-	    	}
-	    }
-	    public void displayBook() {
-	    	System.out.println("Address Books are:");
-	    	for(Map.Entry e : book.entrySet()) {
-	        	System.out.println(e.getKey()+"--> "+e.getValue());
-	        }
-	    }
+    	}
 	}
+    public void addContact()  {
+    	System.out.println("Enter how many contacts you want to add");
+    	int numOfContacts = sc.nextInt();
+    	while(numOfContacts > 0) {
+	    	System.out.println("Enter Name of person");
+			String firstName = sc.next();
+	    	if(detailsBook.containsKey(firstName)) {
+	    		System.out.println("Contact already exists!");
+	    		return;
+	    	}
+	    	else {
+	    		infoContact.info();
+	    		detailsBook.put(firstName,new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email));
+	    		numOfContacts--;
+	    	}
+    	}
+    }
+    public void editContact()  {
+    	System.out.println("Enter name of person whose contact you want to edit");
+			String firstName = sc.next();
+	    	if(detailsBook.containsKey(firstName)) {
+	        	infoContact.info();
+	    		detailsBook.put(firstName,new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email));
+	    	}
+	    	else {
+	    		System.out.println("Contact is not present in book");
+	    		return;
+	    	}
+    }
+    public void deleteContact()  {
+    	System.out.println("Enter name of person whose contact you want to delete");
+			String name = sc.next();
+	    	if(detailsBook.containsKey(name)) {
+	    		detailsBook.remove(name);
+	    	}
+	    	else {
+	    		System.out.println("Contact is not present in book");
+	    		return;
+	    	}
+    }   
+    public void display() {
+        System.out.println("Created contact list is");
+        for(Map.Entry m : detailsBook.entrySet()) {
+        	System.out.println(m.getKey()+"--> "+m.getValue());
+        }
+    }
+    public void addToBook() {
+	    	System.out.println("Enter Name of Address Book");
+	    	String bookName = sc.next();
+	    	if(!book.containsKey(bookName)) {
+		    	book.put(bookName, (HashMap<String, Contact>) detailsBook);
+		    	}
+	    	else {
+	    		System.out.println("Book already exists!");
+	    	}
+    }
+    public void operation() {
+    	System.out.println("Enter how many address books you want to add");
+    	int noOfAddressBook = sc.nextInt();
+    	while(noOfAddressBook > 0) {
+    		addContact();
+    		addToBook();
+    		noOfAddressBook--;
+    	}
+    }
+    public void displayBook() {
+    	System.out.println("Address Books are:");
+    	for(Map.Entry e : book.entrySet()) {
+        	System.out.println(e.getKey()+"--> "+e.getValue());
+        }
+    }
+   
+}
