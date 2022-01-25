8238509b86aa0132e9184464336457c630e28ef4
Nandini Yarra <yarranandhini1416@gmail.com>
2022-01-25 09:50:09 +0530

    UPDATED:read and write contact as csv file

--- src/main/java/com/bridgelabz/AddressBookCollections/InterFaceOne.java
+++ src/main/java/com/bridgelabz/AddressBookCollections/InterFaceOne.java
@@ -1,22 +1,31 @@
 package main.java.com.bridgelabz.AddressBookHashMap;
 
+import java.io.IOException;
+import java.util.List;
+
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+
 public interface InterFaceOne {
-	void createContact();
-	void addContact();
-	void editContact();
-	void deleteContact();
-	void display();
-	void displayBook();
-	void addToBook();
-	void operation();
-	void searchPerson();
-	void addContactByCity();
-	void viewByCity();
-	void addContactByState();
-	void viewByState();
-	void sortAddressBook();
-	void sortByCity();
-	void sortByState();
-	void writeToAddressBookFile();
-	public List<String> readDataFromFile();
+	
+		void createContact();
+		void addContact();
+		void editContact();
+		void deleteContact();
+		void display();
+		void displayBook();
+		void addToBook();
+		void operation();
+		void searchPerson();
+		void addContactByCity();
+		void viewByCity();
+		void addContactByState();
+		void viewByState();
+		void sortAddressBook();
+		void sortByCity();
+		void sortByState();
+		void writeToAddressBookFile();
+		public List<String> readDataFromFile();
+		public void writeDataToCSV() throws IOException, CsvRequiredFieldEmptyException, CsvDataTypeMismatchException;
+		public <CsvValidationException extends Throwable> void readDataFromCSV() throws IOException, CsvValidationException;
 }
